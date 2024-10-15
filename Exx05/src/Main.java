6cddb36990c828c57acb8d341b25982ef646de8a
luzianerp <95301530+luzianerp@users.noreply.github.com>
2024-10-15 15:28:39 -0300

    Add files via upload

---
+++ Exx05/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Aluno meuAluno = new Aluno();
+        meuAluno.nome = "Akemi";
+        meuAluno.idade = 18;
+
+        meuAluno.exibeInformacoes();
+
+    }
+}
