ef0b715d4bbbb5452479eddfb5dc78429088d48e
luzianerp <95301530+luzianerp@users.noreply.github.com>
2024-10-15 15:28:22 -0300

    Add files via upload

---
+++ ex02/src/Calculadora.java
@@ -0,0 +1,5 @@
+public class Calculadora {
+    public int dobrarNumero(int numero) {
+        return numero * 2;
+    }
+}
---
+++ ex02/src/Main.java
@@ -0,0 +1,11 @@
+/*Crie uma classe Calculadora
+com um método que recebe um número como parâmetro e retorna o dobro desse número.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.dobrarNumero(5);
+        System.out.println(resultado);
+    }
+
+}
